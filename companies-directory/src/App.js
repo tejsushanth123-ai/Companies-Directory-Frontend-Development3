7b30819ab1df5dac416b6260af64fdf1e904cdaa
Kavala Tej Sushanth <tejsushanth123@gmail.com>
2025-11-21 23:00:59 +0530

    Update App.js

--- companies-directory/src/App.js
+++ companies-directory/src/App.js
@@ -7,7 +7,6 @@ function App() {
     <div className="app-container">
       <div className="title-card">
         <h1>Companies Directory</h1>
-        <p>Explore and manage the list of all registered organizations.</p>
       </div>
 
 
